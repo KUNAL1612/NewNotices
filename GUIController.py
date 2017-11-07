391b0e512f99c93fc1e1068bc46c66a8107f2411
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2017-11-07 09:45:26 +0530

    Bug Fixes
    Fixed a bug where only a part of big notices would be shown due to the static size of the frame module of Tkinter. Used MessageBox instead to create a dynamically sized window to display notices of any word count.

--- GUIController.py
+++ GUIController.py
@@ -1,19 +1,23 @@
-from tkinter import *
+import tkinter
+import tkinter.messagebox
 
 # this function takes a input text,creates an independent frame and displays the text on desktop
 
 
 def display(txt="this is a dummy text"):
-    root = Tk()
+    root = tkinter.Tk()
+    root.withdraw()
 
     # this block of code generates a simple frame, sets its dimension
-    root.title("new update!")
-    root.geometry('300x100')
+    #root.title("new update!")
+    #root.geometry('300x100')
     # code the desired location for the frame
 
-    app = Frame(root)
-    app.grid()
+    #app = tkinter.Frame(root)
+    #app.grid()
     # feeding the text received as parameter
-    label = Label(app, text=txt)
-    label.grid()
-    root.mainloop()
+    #label = tkinter.Label(app, text=txt)
+    #label.grid()
+    #root.mainloop()
+    
+    tkinter.messagebox.showinfo("NEW NOTICE!",txt)
