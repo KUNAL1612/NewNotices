47f8d02fb1ebbbe4fc8351939294dea7d721085c
Kunal Chakrabarty <KUNAL1612@users.noreply.github.com>
2017-11-07 09:46:04 +0530

    Simplified the code

--- GUIController.py
+++ GUIController.py
@@ -7,17 +7,4 @@ import tkinter.messagebox
 def display(txt="this is a dummy text"):
     root = tkinter.Tk()
     root.withdraw()
-
-    # this block of code generates a simple frame, sets its dimension
-    #root.title("new update!")
-    #root.geometry('300x100')
-    # code the desired location for the frame
-
-    #app = tkinter.Frame(root)
-    #app.grid()
-    # feeding the text received as parameter
-    #label = tkinter.Label(app, text=txt)
-    #label.grid()
-    #root.mainloop()
-    
     tkinter.messagebox.showinfo("NEW NOTICE!",txt)
